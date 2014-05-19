537ffeab68d0148bd54303395178696ee249d301
Robert Talamantez <rtalamantez@localhost.localdomain>
2014-05-19 16:42:49 -0700

    minified a bit

--- myNotesApp/app/js/app/app.js
+++ myNotesApp/app/js/app/app.js
@@ -44,39 +44,30 @@ define([
                 return this;
             }
         });
-
         //CREATE EMPLOYEES
         var robert = new EmployeeModel({
             firstName: "Robert",
             lastName: "Talamantez",
             position: "Coder"
         });
-
         var xerxes = new EmployeeModel({
             firstName: "xerxes",
             lastName: "jones",
             position: "Vice-President of Conquering"
-
         });
         var robertView = new EmployeeView({
             model: robert
         });
-
         //robertView.render();
-
         var xerxesView = new EmployeeView({
             model: xerxes
         });
-
         //xerxesView.render();
         //robert.set("position", "punchcard jockey");
-
-
         //CREATE EMPLOYEES COLLECTION
         var Employees = Backbone.Collection.extend({
             model: EmployeeModel
         });
-
         var employees = new Employees();
         employees.add(robert);
         employees.add(xerxes);
@@ -92,7 +83,6 @@ define([
                 this.$el.html(this.template(this.collection.toJSON()));
             }
         });
-
         //Instatiate EmployeesView
         var employeesView = new EmployeesView({
             collection: employees
@@ -100,7 +90,4 @@ define([
         //render employees
         employeesView.render();
     });
-
-
-
 });
